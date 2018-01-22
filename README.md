0837526b2cf1d32282e0bc3d2b2c7b8de99a7bd8
laurengranada <laurengranada@utexas.edu>
2018-01-21 21:05:02 -0600

    adding updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # week-4-game
+
+### Overview
+
+Created a crystal collector game, each crystal image holds a certain value every round of the game. Using Javascript I made a loop that constantly compares the player's total score to the randomized value that they are aiming to match.'
+
+### Languages Used
+
+HTML, CSS, Javascript
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,84 @@
 $(document).ready(function() {
 
-
+//setting variables
 var score = 0;
 var wins = 0 ;
 var losses = 0;
 
+//allow html to show variables
 $("#score").html(score);
 
+//randomizes the target number player needs to get
 var random = Math.floor(Math.random()* ((120-19)+1) + 19);
-	$("#random").html(random);
+	$("#matchNumber").html(random);
 
+// starts new game
 function newGame(){
 	var random = Math.floor(Math.random()* ((120-19)+1) + 19);
-	$("#random").html(random);
+	$("#matchNumber").html(random);
 	score = 0;
 	$("#score").html(score);
 	var red = Math.ceil(Math.random()* 12);
 	var blue = Math.ceil(Math.random()* 12);
 	var pink = Math.ceil(Math.random()* 12);
 	var sky = Math.ceil(Math.random()* 12);
-
 }
 
-var red = Math.ceil(Math.random()* 12);
-var blue = Math.ceil(Math.random()* 12);
-var pink = Math.ceil(Math.random()* 12);
-var sky = Math.ceil(Math.random()* 12);
+	var red = Math.ceil(Math.random()* 12);
+	var blue = Math.ceil(Math.random()* 12);
+	var pink = Math.ceil(Math.random()* 12);
+	var sky = Math.ceil(Math.random()* 12);
 
 $("#red").on("click", function(){
 	 score = score + red;
 	console.log("score" + red);
 	$("#score").html(score);
-	if (random === score){
-		wins++;
-		$("#win").html(wins);
-		$("#comment").html("You win");
-		newGame();
-	}else if(score > random){
-		losses++;
-		$("#loss").html(losses);
-		$("#comment").html("You lost");
-		newGame();
-	}
-	
-})
+	compare();
+
+});
 
 $("#blue").on("click", function(){
 	 score = score + blue;
 	console.log("score" + blue);
 	$("#score").html(score);
-	if (random === score){
-		wins++;
-		$("#win").html(wins);
-		$("#comment").html("You win");
-		newGame();
-	}else if(score > random){
-		losses++;
-		$("#loss").html(losses);
-		$("#comment").html("You lost");
-		newGame();
-	}
-})
+	compare();
+
+});
 
 $("#pink").on("click", function(){
 	 score = score + pink;
 	console.log("score" + pink);
 	$("#score").html(score);
-	if (random === score){
-		wins++;
-		$("#win").html(wins);
-		$("#comment").html("You win");
-		newGame();
-	}else if(score > random){
-		losses++;
-		$("#loss").html(losses);
-		$("#comment").html("You lost");
-		newGame();
-	}
-})
+	compare();
+
+});
 
 $("#sky").on("click", function(){
 	 score = score + sky;
 	console.log("score" + sky);
 	$("#score").html(score);
+	compare();
+
+});
+
+function compare(){
 	if (random === score){
 		wins++;
-		$("#win").html(wins);
+		$("#wins").html(wins);
 		$("#comment").html("You win");
 		newGame();
 	}else if(score > random){
 		losses++;
-		$("#loss").html(losses);
+		$("#losses").html(losses);
 		$("#comment").html("You lost");
 		newGame();
-	}
-})
+}
 
+};
 
 
 
 });
+
+
+
